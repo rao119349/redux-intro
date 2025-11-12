ad688a56069f7da47a9e2c555fc2122f760ec02f
rao119349 <sauravyadav1193@gmail.com>
2025-11-12 22:59:56 +0530

    changes

--- src/store-v2.js
+++ src/store-v2.js
@@ -8,7 +8,6 @@ import { composeWithDevTools } from "redux-devtools-extension";
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
-  
 });
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)) );
 
